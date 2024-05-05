327862ee2aa8ce463953daf70ac7743c3f4b5703
Prasanna Raut <prasannaraut36@gmail.com>
2024-05-05 12:33:45 +0530

    Update Template_BinaryClassification.py

--- Template_BinaryClassification.py
+++ Template_BinaryClassification.py
@@ -285,7 +285,7 @@ loss_model = "binary_crossentropy"
 metrics_to_be_used = ["accuracy"]
 
 model.compile(optimizer=optimizer,loss=loss_model, metrics=metrics_to_be_used)
-model.summary()
+
 
 from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1,
@@ -296,6 +296,8 @@ history = model.fit(x=train_inputs_array,y=train_labels_array,
                     epochs = 500,
                     shuffle = True,
                     callbacks=[reduce_lr])
+
+model.summary()
 
 variables_for_plot = ["loss"] + metrics_to_be_used
 

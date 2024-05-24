09460ffa1bf7a0418f993efffcdc6d48e7d7e28c
Prasanna Raut <prasannaraut36@gmail.com>
2024-05-24 19:57:57 +0530

    Add files via upload

---
+++ tf2p13p0_kerasCV_stableDiffusion.py
@@ -0,0 +1,20 @@
+# source: https://www.tensorflow.org/tutorials/generative/generate_images_with_stable_diffusion
+
+import time
+import keras_cv
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+
+model = keras_cv.models.StableDiffusion(img_width=512, img_height=512)
+
+images = model.text_to_image("Photograph of an astronaut riding a horse", batch_size=3)
+
+def plot_images(images):
+    plt.figure(figsize=(20,20))
+    for i in range(len(images)):
+        ax = plt.subplot(1, len(images), i+1)
+        plt.imshow(images[i])
+        plt.axis('off')
+
+plot_images(images)

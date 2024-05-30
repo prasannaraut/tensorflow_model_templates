da3146b8f9f8fd9c7f54530195a2b4fc3b7625c3
Prasanna Raut <prasannaraut36@gmail.com>
2024-05-30 20:58:55 +0530

    Update tf2p13p0_Template_TimeSeries_multiIn_multiOut.py

--- tf2p13p0_Template_TimeSeries_multiIn_multiOut.py
+++ tf2p13p0_Template_TimeSeries_multiIn_multiOut.py
@@ -27,6 +27,7 @@ class process_data_for_time_series_model():
         self.all_cols = list(set(input_cols+label_columns))
         self.input_cols = input_cols
         self.label_columns = label_columns
+        self.gap = gap
         # Store the raw data.
         if train_df is not None:
             self.train_df = train_df[self.all_cols]
@@ -92,6 +93,17 @@ class process_data_for_time_series_model():
 
         return inputs, labels
 
+    # helper function for final prediction in the future values
+    def split_window_only_inputs(self, features):
+        inputs = features[:, self.input_slice, :]
+
+        # Slicing doesn't preserve static shape information, so set the shapes
+        # manually. This way the `tf.data.Datasets` are easier to inspect.
+        inputs.set_shape([None, self.input_width, None])
+
+
+        return inputs
+
     # convert dataframe to formatted tensor
     def make_dataset(self, data):
         data = np.array(data, dtype=np.float32)
@@ -100,63 +112,121 @@ class process_data_for_time_series_model():
             targets=None,
             sequence_length=self.total_window_size,
             sequence_stride=1,
-            shuffle=True,
+            shuffle=False,
             batch_size=32, )
 
         ds = ds.map(self.split_window)
 
         return ds
 
+    # convert dataframe to formatted tensor only inputs
+    def make_dataset_only_inputs(self, data):
+        data = np.array(data, dtype=np.float32)
+        ds = tf.keras.utils.timeseries_dataset_from_array(
+            data=data,
+            targets=None,
+            sequence_length=self.input_width,
+            sequence_stride=1,
+            shuffle=False,
+            #batch_size=32,
+        )
+
+        ds = ds.map(self.split_window_only_inputs)
+
+        return ds
 
 
     def plot_train_data(self, cols=None):
         if cols is None:
             cols = self.input_cols
-        if train_df is not None:
-            plt.figure(figsize=(12,8))
+
+        if self.train_df is not None:
+            plt.figure(figsize=(12, 8))
             for c in cols:
                 plt.plot(self.train_df.index, self.train_df[c], label=c)
 
             plt.xlabel("Time")
             plt.title("Train data frame plots")
+            plt.legend(loc="upper left")
             plt.plot()
 
-    def plot_valid_data(self, output):
-        valid_df = self.valid_df
-        output_cols = self.label_columns
-        i = 0
-        for c in output_cols:
-            last_point_data_x = valid_df.index[-1 * output.shape[1]:]
-            last_point_data_y = output[-1 -1*output.shape[1], :, :][:, i]
+    # note cross check the logic of plots once again
+    def plot_train_data_withPredictions(self, train_output_predictions):
 
-            plt.figure(figsize=(12, 8))
-            plt.plot(valid_df.index, valid_df[c], label=c)
-            plt.scatter(last_point_data_x, last_point_data_y, label="Predicted")
+        train_df_toPlot = self.train_df.iloc[self.input_width-1:-1*self.label_width-1*self.gap, :]
+        if self.train_df is not None:
+            count_label=0
+            for c_out in self.label_columns:
+                plt.figure(figsize=(12, 8))
+                plt.plot(train_df_toPlot.index, train_df_toPlot[c_out], label=c_out)
+                for i in range(train_output_predictions.shape[1]):
+                    plt.plot(train_df_toPlot.index, train_output_predictions[:, i, count_label], label="{}th prediction".format(i), linestyle='dashed')
+                plt.xlabel("Time")
+                plt.title("Train data frame plots for output column as {}".format(c_out))
+                plt.legend(loc="upper left")
+                plt.plot()
+                count_label=count_label+1
 
-            plt.xlabel("Time")
-            plt.title("validation data frame plot for column {}".format(c))
-            plt.show
+    # note cross check the logic of plots once again
+    def plot_valid_data_withPredictions(self, valid_output_predictions):
 
-            i = i + 1
+        valid_df_toPlot = self.valid_df.iloc[self.input_width-1:-1*self.label_width-1*self.gap, :]
+        if self.valid_df is not None:
+            count_label=0
+            for c_out in self.label_columns:
+                plt.figure(figsize=(12, 8))
+                plt.plot(valid_df_toPlot.index, valid_df_toPlot[c_out], label=c_out)
+                for i in range(train_output_predictions.shape[1]):
+                    plt.plot(valid_df_toPlot.index, valid_output_predictions[:, i, count_label], label="{}th prediction".format(i), linestyle='dashed')
+                plt.xlabel("Time")
+                plt.title("Valid data frame plots for output column as {}".format(c_out))
+                plt.legend(loc="upper left")
+                plt.plot()
+                count_label=count_label+1
+
+    #note cross check the logic of plots once again
+    def plot_test_data_withPredictions(self, test_output_predictions):
+
+        test_df_toPlot = self.test_df.iloc[self.input_width-1:-1*self.label_width-1*self.gap, :]
+        if self.valid_df is not None:
+            count_label=0
+            for c_out in self.label_columns:
+                plt.figure(figsize=(12, 8))
+                plt.plot(test_df_toPlot.index, test_df_toPlot[c_out], label=c_out)
+                for i in range(train_output_predictions.shape[1]):
+                    plt.plot(test_df_toPlot.index, test_output_predictions[:, i, count_label], label="{}th prediction".format(i), linestyle='dashed')
+                plt.xlabel("Time")
+                plt.title("Test data frame plots for output column as {}".format(c_out))
+                plt.legend(loc="upper left")
+                plt.plot()
+                count_label=count_label+1
 
 
-    def plot_test_data(self, output):
-        test_df = self.test_df
-        output_cols = self.label_columns
-        i = 0
-        for c in output_cols:
-            last_point_data_x = test_df.index[-1 * output.shape[1]:]
-            last_point_data_y = output[-1 -1*output.shape[1], :, :][:, i]
+    #note cross check the logic of plots once again
+    def plot_future_predictions(self, future_predictions, train_mean, train_std):
 
-            plt.figure(figsize=(12, 8))
-            plt.plot(test_df.index, test_df[c], label=c)
-            plt.scatter(last_point_data_x, last_point_data_y, label="Predicted")
+        df = pd.concat([self.train_df, self.valid_df, self.test_df])
+        df = df[self.label_columns]
 
-            plt.xlabel("Time")
-            plt.title("test data frame plot for column {}".format(c))
-            plt.show
+        df_len = len(df)
 
-            i = i + 1
+        for i in range(future_predictions.shape[1]):
+            df.loc[df_len+i]=future_predictions[0,i,:]
+        df=df.reset_index()
+
+        if df is not None:
+            count_label=0
+            for c_out in self.label_columns:
+                mean = train_mean[c_out]
+                std = train_std[c_out]
+                plt.figure(figsize=(12, 8))
+                plt.plot(df.index[:-3], df[c_out][:-3]*std+mean, label=c_out)
+                plt.plot(df.index[-3:], df[c_out][-3:]*std+mean, label=c_out)
+                plt.xlabel("Time")
+                plt.title("Final Prediction output column as {}".format(c_out))
+                plt.legend(loc="upper left")
+                plt.plot()
+                count_label=count_label+1
 
 ##############################################################################################################################
 # df = pd.read_csv("datasets/PET_PRI_GND_DCUS_NUS_W.csv", infer_datetime_format=True, index_col='Date', header=0)
@@ -186,20 +256,38 @@ df["y"] = y
 
 
 ### Define Parameters
+history_points=10
+future_points=3
+gap=0
+input_cols = ["A1","A2","A3"]
+output_cols = ["A1","A2"]
+
+
 total_length = len(df)
 train_df = df[ : int(total_length*0.7)]
 valid_df = df[int(total_length*0.7) : int(total_length*0.9)]
 test_df = df[int(total_length*0.9) : ]
+future_prediction_df = df[total_length-history_points+1 :]
 
-history_points=10
-future_points=3
-gap=0
-input_cols = list(train_df.columns)
-output_cols = ["A1"]
+
+
+## Normalization of data
+train_mean = train_df.mean()
+train_std = train_df.std()
+
+train_df = (train_df - train_mean) / train_std
+valid_df = (valid_df - train_mean) / train_std
+test_df = (test_df - train_mean) / train_std
+
 
 # Process the data
 w1 = process_data_for_time_series_model(history_points=history_points, future_points=future_points, gap=gap, input_cols=input_cols,  label_columns=output_cols, train_df=train_df, valid_df=valid_df, test_df=test_df)
 #data = w1.get_data()
+
+
+
+
+
 
 # Get Tensors
 if train_df is not None:
@@ -209,14 +297,19 @@ if valid_df is not None:
 if test_df is not None:
     test_ds = w1.make_dataset(test_df)
 
+if future_prediction_df is not None:
+    future_prediction_ds = w1.make_dataset_only_inputs(future_prediction_df)
+
+
+
+
 
 ########## Create models
 lstm_model = tf.keras.models.Sequential([
     # Shape [batch, time, features] => [batch, time, lstm_units]
-    tf.keras.layers.LSTM(128, return_sequences=True),
-    tf.keras.layers.LSTM(32),
-    tf.keras.layers.Dense(32),
-    tf.keras.layers.Dense(16),
+    tf.keras.layers.LSTM(32, return_sequences=True),
+    tf.keras.layers.LSTM(4),
+    tf.keras.layers.Dense(4),
     # Shape => [batch, time, features]
     tf.keras.layers.Dense(future_points * len(output_cols),
                           kernel_initializer=tf.initializers.zeros()),
@@ -239,7 +332,7 @@ early_stopping = EarlyStopping(patience=15)
 
 history = model.fit(train_ds,
                     validation_data=valid_ds,
-                    epochs=100,
+                    epochs=1000,
                     callbacks=[reduce_lr, early_stopping])
 
 metrics_to_be_used = []
@@ -259,12 +352,18 @@ for var in variables_for_plot:
     plt.show()
 
 # get predictions
+
+train_output_predictions = model.predict(train_ds)
 valid_output_predictions = model.predict(valid_ds)
 test_output_predictions = model.predict(test_ds)
+future_predictions = model.predict(future_prediction_ds)
 
 # create prediction plots
-w1.plot_valid_data(valid_output_predictions)
-w1.plot_test_data(test_output_predictions)
+w1.plot_train_data()
+w1.plot_train_data_withPredictions(train_output_predictions)
+w1.plot_valid_data_withPredictions(valid_output_predictions)
+w1.plot_test_data_withPredictions(test_output_predictions)
+w1.plot_future_predictions(future_predictions, train_mean, train_std)
 
 
 
